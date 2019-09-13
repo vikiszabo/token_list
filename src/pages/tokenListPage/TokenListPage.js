af341b25086576ec9dbf7a1f8929e87f61022f6f
weiliya <viktoria.szabo.cv@gmail.com>
2019-09-13 21:52:03 +0200

    menu and layout created

--- src/pages/tokenListPage/TokenListPage.js
+++ src/pages/tokenListPage/TokenListPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './Style.css';
-import { Layout, Menu, Icon } from 'antd';
+import {Icon, Layout, Menu} from 'antd';
 import LOGO from '../../logo.svg';
 
 
@@ -10,25 +10,25 @@ function TokenListPage() {
     const {Sider, Content} = Layout;
 
     return (
-        <Layout>
-            <Sider width={'25%'}>
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                    <Menu.Item key="1">
-                        <img alt={''} src={LOGO} width={50} height={50}/>
+        <Layout style={{height: 1200, width: 1600}}>
+            <Sider type={'flex'} justify={'center'} width={'25%'} >
 
-                    </Menu.Item>
-                    <Menu.Item key="2">
+                <div className='logo'>
+                    <img alt={''} src={LOGO} width={50} height={50}/>
+                </div>
+                <Menu  mode={'inline'} defaultSelectedKeys={['2']}>
+                    <Menu.Item  key="1">
                         <Icon type="plus" />
                         <span>Issue Token</span>
                     </Menu.Item>
-                    <Menu.Item key="3">
+                    <Menu.Item key="2">
                         <Icon type="unordered-list" />
                         <span>Token List</span>
                     </Menu.Item>
                 </Menu>
 
             </Sider>
-            <Content width={'75%'}> Content
+            <Content width={'75%'} > Content
             </Content>
         </Layout>
     )
